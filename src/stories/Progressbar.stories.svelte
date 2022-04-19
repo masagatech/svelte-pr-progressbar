9e0ba911d9396c6bec07dcd4023bf10bb480d9c9
masagatech <pratikway.90@gmail.com>
2022-04-19 23:12:52 +0530

    first commit

---
+++ src/stories/Progressbar.stories.svelte
@@ -0,0 +1,37 @@
+<script>
+// @ts-nocheck
+
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import Progressbar from "./Progressbar.svelte";
+</script>
+
+<!-- More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export -->
+<!-- More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes -->
+<Meta
+  title="Example/Progressbar"
+  component={Progressbar}
+  argTypes={{
+    backgroundColor: { control: "color" },
+    color: { control: "color" },
+    height: { control: "text" }
+    // size: {
+    //   control: { type: 'select' },
+    //   options: ['small', 'medium', 'large'],
+    // },
+  }}
+/>
+
+<!-- More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args -->
+<Template let:args>
+  <Progressbar {...args} />
+</Template>
+
+<!-- More on args: https://storybook.js.org/docs/svelte/writing-stories/args -->
+<Story
+  name="Bar"
+  args={{  
+     
+  }}
+/>
+ 
+
---
+++ src/stories/Progressbar.svelte
@@ -0,0 +1,58 @@
+<script>
+	import './Progressbar.css';
+
+	export let height = '10px';
+
+	export /**
+	 * @type {any}
+	 */ let backgroundColor = '#f5f5f5';
+
+	export /**
+	 * @type {any}
+	 */ let color = '#5681d3';
+
+	export let progress = 0;
+
+	export let active = false;
+
+	export let indeterminate = false;
+	export let showPercent = true;
+
+	let bgstyle = backgroundColor ? `background-color: ${backgroundColor}` : '';
+	let style = color ? `background-color: ${color}` : '';
+	style += ';height:' + height;
+
+	$: {
+		if (indeterminate) {
+		}
+	}
+
+	$: {
+		if (progress >= 0 && progress <= 100) {
+			style += ';' + 'width:' + (indeterminate ? 0 : progress) + '%;';
+		} else {
+			progress = 100;
+		}
+	}
+</script>
+
+<div class="progressbar-holder">
+	<div class="progressbar-segments">
+		<div class="progressbar-innersegment">
+			<div class="progressbar-outer" style={bgstyle}>
+				<div
+					class="progressbar-inner {active && !indeterminate ? 'active' : ''} {indeterminate
+						? 'indeterminate'
+						: ''}"
+					{style}
+				/>
+			</div>
+		</div>
+		{#if showPercent}
+			<span class="progressbar-text">
+				{progress}%
+			</span>
+		{/if}
+	</div>
+</div>
+ 
